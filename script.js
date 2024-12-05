8d552ac10994514c1883ac5d851e8b8e4578172f
Elton Esteves <121466168+waesteves2@users.noreply.github.com>
2024-12-05 11:30:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -184,7 +184,6 @@ function gerarDocumento() {
 <div style="border: 1px solid #000; padding: 10px;">
 <h4>PAGADOR DE FRETE REMETENTE</h4>
 <div style="text-align: center;">
-  ${cobrarFrete ? `<p>Valor do Frete: R$ ${freteValor.toFixed(2)}</p>` : `<p></p>`}
 
   <p>____________________________________</p>
   ASSINATURA DO REMETENTE
